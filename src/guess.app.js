f067d8834a073380a95515e58324c23901952042
Sam Jespersen <heyheysamj@gmail.com>
2019-07-16 18:13:36 -0700

    added extraneous feature for visual feedback for guessed number

--- src/guess.app.js
+++ src/guess.app.js
@@ -2,20 +2,20 @@ import compareNumbers from './compareNumbers.js';
 
 const submitButton = document.getElementById('submit');
 const numberInput = document.getElementById('number');
+const pointer = document.getElementById('pointer');
 
 let guesses = 4;
 
 let targetNumber = Math.floor(Math.random() * 20) + 1; //source: verified by Luke
 
 submitButton.addEventListener('click', () => {
-    
-    if(guesses > 0)
-    {
+
+    if(guesses > 0) {
         let userGuess = compareNumbers(numberInput.value, targetNumber);
 
-        if(userGuess === 1){
+        if(userGuess === 1) {
             console.log('Too high!');
-        } else if(userGuess === -1){
+        } else if(userGuess === -1) {
             console.log('Too low!');
         } else {
             console.log('You got it!');
@@ -25,4 +25,16 @@ submitButton.addEventListener('click', () => {
     } else {
         console.log('Sorry! No more guesses!');
     }
+
+    movePointer(numberInput.value);
 });
+
+function movePointer(num) {
+
+    let pointerPosition = 2;
+    if(num === 1) {
+        pointer.style.margin = '0 0 0 2px';
+    } else {
+        pointer.style.margin = `0 0 0 ${2 + (58 * (num - 1))}px`;
+    }
+}
