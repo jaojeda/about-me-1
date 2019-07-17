a636f4adc40901d4e5198032087aad14f2234e68
Sam Jespersen <heyheysamj@gmail.com>
2019-07-16 22:43:46 -0700

    added guesses remaining display

--- src/guess.app.js
+++ src/guess.app.js
@@ -6,6 +6,8 @@ const pointer = document.getElementById('pointer');
 
 let guesses = 4;
 
+setTextContent('guesses', `Ahh jeez you only have ${guesses} guesses left!!`);
+
 let targetNumber = Math.floor(Math.random() * 20) + 1; //source: verified by Luke
 
 submitButton.addEventListener('click', () => {
@@ -13,26 +15,46 @@ submitButton.addEventListener('click', () => {
     let userGuessAnswer = compareNumbers(userGuess, targetNumber);
     movePointer(userGuess);
 
-    if(userGuessAnswer === 1) {
+    if (userGuessAnswer === 1) {
         setTextContent('msg', 'Too high!');
         elimRange(userGuess, 'up');
 
-    } else if(userGuessAnswer === -1) {
+    } else if (userGuessAnswer === -1) {
         setTextContent('msg', 'Too low!');
         elimRange(userGuess, 'down');
 
     } else {
         setTextContent('msg', 'You got it!');
         toggleClass(userGuess, 'win', 'add');
+        setTextContent('guesses', 'WOWWY ZOWWY YOU DID IT!! GREAT JORB');
+        submitButton.disabled = true;
     }
 
     guesses--;
 
-    if(guesses === 0) { submitButton.disabled = true; }
+    switch (guesses) {
+        case 3:
+            setTextContent('guesses', `Oh no oh man you only have ${guesses} guesses left now!!!`);
+            break;
+        case 2:
+            setTextContent('guesses', `Ohhhhh god you only have ${guesses} guesses remaining! you can do it!!!!`);
+            break;
+        case 1:
+            setTextContent('guesses', `FFFFFfffhghghghg only ${guesses} guess left!!! ITS NOW OR NEVER`);
+            break;
+        case 0:
+            setTextContent('guesses', `OH NO YOU LOST. THE ANSWER WAS ${targetNumber} LMAO RIP`);
+            break;
+
+    }
+
+
+
+    if (guesses === 0) { submitButton.disabled = true; }
 });
 
 function movePointer(num) {
-    if(num === 1) {
+    if (num === 1) {
         pointer.style.margin = '0 0 0 2px';
     } else {
         pointer.style.margin = `0 0 0 ${2 + (58 * (num - 1))}px`;
@@ -40,13 +62,13 @@ function movePointer(num) {
 }
 
 function elimRange(num, dir) {
-    if(dir === 'down') {
-        for(let i = 1; i <= num; i++) {
+    if (dir === 'down') {
+        for (let i = 1; i <= num; i++) {
             toggleClass(i, 'elim', 'add');
             toggleClass(i, 'pos', 'remove');
         }
     } else {
-        for(let j = num; j <= 20; j++) {
+        for (let j = num; j <= 20; j++) {
             toggleClass(j, 'elim', 'add');
             toggleClass(j, 'pos', 'remove');
         }
@@ -59,7 +81,7 @@ function setTextContent(id, string) {
 
 function toggleClass(id, elementClass, action) {
 
-    if(action === 'add') {
+    if (action === 'add') {
         document.getElementById(id).classList.add(elementClass);
     } else {
         document.getElementById(id).classList.remove(elementClass);
