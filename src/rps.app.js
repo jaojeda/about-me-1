e8d161e709df114681018d04ba8f721cf70c247b
Sam Jespersen <heyheysamj@gmail.com>
2019-07-18 12:13:19 -0700

    Made pictures into buttons, hid elements until the gameplay, and added navigation

--- src/rps.app.js
+++ src/rps.app.js
@@ -1,46 +1,49 @@
 import getThrow from './get-throw.js';
 import didPlayerWin from './get-play.js';
 
-const submitButton = document.getElementById('submit');
 const playerChoice = document.getElementById('player-choice');
 const compChoice = document.getElementById('comp-choice');
 const winDisplay = document.getElementById('wins');
 const lossDisplay = document.getElementById('loss');
 const tieDisplay = document.getElementById('tie');
+const result = document.getElementById('result');
+const shoot = document.getElementById('shoot');
 
 let win = 0;
 let loss = 0;
 let tie = 0;
 
-submitButton.addEventListener('click', () => {
-    const computerThrow = getThrow();
+const inputArray = [...document.querySelectorAll('input')];
 
-    const userInput = document.querySelector('input:checked');
-    playerChoice.src = `assets/${userInput.value}.png`;
-    compChoice.src = `assets/${computerThrow}.png`;
+inputArray.forEach((elem) => {
+    elem.addEventListener('click', () => {
+        const computerThrow = getThrow();
 
-    const gameResult = didPlayerWin(convertToNum(userInput.value), computerThrow);
-    switch(gameResult) {
-        case 'Win':
-            win++;
-            winDisplay.textContent = win;
-            break;
-        case 'Loss':
-            loss++;
-            lossDisplay.textContent = loss;
-            break;
-        case 'Tie':
-            tie++;
-            tieDisplay.textContent = tie;
-            break;
-    }
+        const userInput = document.querySelector('input:checked');
+        playerChoice.src = `assets/${userInput.value}.png`;
+        compChoice.src = `assets/${computerThrow}.png`;
 
+        const gameResult = didPlayerWin(convertToNum(userInput.value), computerThrow);
+
+        result.classList.remove('hidden');
+        shoot.classList.remove('hidden');
+
+        switch(gameResult) {
+            case 'Win':
+                win++;
+                winDisplay.textContent = win;
+                break;
+            case 'Loss':
+                loss++;
+                lossDisplay.textContent = loss;
+                break;
+            case 'Tie':
+                tie++;
+                tieDisplay.textContent = tie;
+                break;
+        }
+    });
 });
-
-
-
-
-
 
 function convertToNum(playerThrow) {
     switch(playerThrow) {
