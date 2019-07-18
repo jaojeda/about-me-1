e46cdfbf417cdd73297b043ccd3fde535887fd8e
Sam Jespersen <heyheysamj@gmail.com>
2019-07-18 09:17:06 -0700

    getThrow function

---
+++ src/get-throw.js
@@ -0,0 +1,19 @@
+function getThrowFromNumber(num) {
+    switch(num) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+}
+
+
+function getThrow() {
+    let rnum = Math.floor(Math.random() * 3);
+    return getThrowFromNumber(num);
+}
+
+
+export default getThrow;
